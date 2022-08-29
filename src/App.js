41d8f119f82969f661c633b1c4a36310a2d48ed0
Leonardo Melchiori <91997494+leomel266@users.noreply.github.com>
2022-08-29 14:46:25 -0300

    cart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer1 from './components/ItemListContainer1';
+import CartWidget from './components/CartWidget';
 
 function App() {
   return (
     <div className='container'>
       <header className='header'>
         <NavBar/>
+        <CartWidget/>
         <ItemListContainer1 greetings='Saludos desde el contenedor'/>
       </header>
 
---
+++ src/components/CartWidget.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import cart from './img/cart.png'
+
+const CartWidget = () => {
+    return (
+        <div>
+            <img src={cart} alt='cart'/>
+        </div>
+    );
+}
+
+export default CartWidget;
