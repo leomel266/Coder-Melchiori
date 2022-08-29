646be89b04900acc72aa23684ba9d81a84e60087
Leonardo Melchiori <91997494+leomel266@users.noreply.github.com>
2022-08-29 14:35:40 -0300

    container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import ItemListContainer1 from './components/ItemListContainer1';
 
 function App() {
   return (
     <div className='container'>
       <header className='header'>
-        <NavBar />
-
+        <NavBar/>
+        <ItemListContainer1 greetings='Saludos desde el contenedor'/>
       </header>
 
     </div>
---
+++ src/components/ItemListContainer1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ItemListContainer1 = ({greetings}) => {
+    return (
+        <div>
+            <h2>{greetings} </h2>           
+        </div>
+    );
+}
+
+export default ItemListContainer1;
